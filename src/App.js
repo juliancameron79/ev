59d1a3766a3ed5ba68a367414fe60505903c8072
juliancameron79 <juliancameron79@gmail.com>
2021-07-13 16:34:54 +0200

    Basic layout completed for about component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./component/Navbar";
 import Header from "./component/Header";
+import About from "./component/About";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Header></Header>
+      <About></About>
     </>
   );
 }
