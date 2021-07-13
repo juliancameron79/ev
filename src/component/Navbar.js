d000285e0b474fda45202a356e388df0968813fa
juliancameron79 <juliancameron79@gmail.com>
2021-07-13 23:47:30 +0200

    Add brand text and styled nav links

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -7,15 +7,21 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-md navbar-dark fixed-top" id="myScrollspy">
       <div className="navbar-brand">
         <FontAwesomeIcon className="brand-icon" icon={faChargingStation}></FontAwesomeIcon>
+        <span className="brand-text ml-3">EVCN</span>
       </div>
       <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="navbar-collapse collapse" id="navbarsExampleDefault">
-        <ul className="navbar-nav mr-auto">
+        <ul className="navbar-nav ml-auto">
           <li className="nav-item">
             <a className="nav-link" href="#home">
               Home <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#about">
+              about
             </a>
           </li>
           <li className="nav-item">
