4e12c724674815e5db9165376f558083a8281457
juliancameron79 <juliancameron79@gmail.com>
2021-07-14 18:26:18 +0200

    About component completed

--- src/component/About.js
+++ src/component/About.js
@@ -2,12 +2,16 @@ import React from "react";
 
 const About = () => {
   return (
-    <section id="about" className="site-section">
+    <section id="about" className="site-section bg-pattern">
+      <div className="bg-image"></div>
       <div className="container">
-        <div className="row">
+        <div className="row d-flex justify-content-center">
+          <div className="col-12">
+            <div className="display-4 about-heading text-center mb-5">about us</div>
+          </div>
           <div className="col-6 col-md-6"></div>
-          <div className="col-12 col-md-6">
-            <p className="about-text text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos at vel voluptatum illum, nesciunt id, quisquam excepturi asperiores rerum dolorem eaque, fugit numquam iure alias voluptatibus facere et pariatur itaque animi consequatur ducimus repellat deserunt quibusdam! Sint nobis velit culpa!</p>
+          <div className="col-12 col-md-10">
+            <p className="about-text shadow">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos at vel voluptatum illum, nesciunt id, quisquam excepturi asperiores rerum dolorem eaque, fugit numquam iure alias voluptatibus facere et pariatur itaque animi consequatur ducimus repellat deserunt quibusdam! Sint nobis velit culpa!</p>
           </div>
         </div>
       </div>
