6446a4404ac1ab549a21c75b68c0f2859dfe6240
juliancameron79 <juliancameron79@gmail.com>
2021-07-14 23:31:50 +0200

    Added map marker svg

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
@@ -8,14 +10,17 @@ const Header = () => {
         <div className="container main-info">
           <div className="row about-row d-flex justify-content-center">
             <div className="col-6 d-flex justify-content-center">
-              <div className="display-1 header text-white text-center">Welcome to our Future</div>
+              <div className="display-1 header text-white text-center">
+                Welcome to our Future
+                <div className="mt-5">
+                  <FontAwesomeIcon className="header-icon" icon={faMapMarkerAlt}></FontAwesomeIcon>
+                </div>
+              </div>
             </div>
             <div className="col-md-7">
               <div className="text-background">
-                <div className="about-text">
-                  <h1 className="header-title">Electric Vehicle Charging Network</h1>
-                  <p className="lead-header-text">Our aim is to faciliate EV owners with an up to date charging station network, that will provide you with charging stations in your area.</p>
-                </div>
+                <h1 className="header-title">Electric Vehicle Charging Network</h1>
+                <p className="lead-header-text">Our aim is to faciliate EV owners with an up to date charging station network, that will provide you with charging stations in your area.</p>
               </div>
             </div>
           </div>
