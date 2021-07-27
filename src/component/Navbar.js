a969de5d63cb9a6b52983dbab8a01fe9732a8dde
juliancameron79 <juliancameron79@gmail.com>
2021-07-27 11:45:48 +0200

    Add link and scroll to navbar component

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-scroll";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChargingStation } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-md navbar-dark fixed-top" id="myScrollspy">
+    <nav className="navbar navbar-expand-md navbar-dark fixed-top">
       <div className="navbar-brand">
         <FontAwesomeIcon className="brand-icon" icon={faChargingStation}></FontAwesomeIcon>
         <span className="brand-text ml-3">EVCN</span>
@@ -15,24 +16,24 @@ const Navbar = () => {
       <div className="navbar-collapse collapse" id="navbarsExampleDefault">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <a className="nav-link" href="#home">
-              Home <span className="sr-only">(current)</span>
-            </a>
+            <Link activeClass="active" className="nav-link" to="home" spy={true} smooth={true} href="#home">
+              Home
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#about">
+            <Link activeClass="active" className="nav-link" to="about" spy={true} smooth={true} href="#about">
               about
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#evcn-map">
+            <Link activeClass="active" className="nav-link" to="map" spy={true} smooth={true} href="#map">
               EVCN Map
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#contact-us">
+            <Link activeClass="active" className="nav-link" to="contact-us" spy={true} smooth={true} href="#contact-us">
               Contact Us
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
